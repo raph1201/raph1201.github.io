9b5d7f4980c4e96b268ade021b3382ed5a55b4e8
Raphaël Julien <raphaeljulien@Mac-de-Raph.local>
2023-05-09 16:35:13 -0400

    row 3

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-function toggleExpand() {
+function toggleExpand1() {
   var div2 = document.querySelector('.box1-left-nav');
   div2.classList.toggle('box1-left-nav-expanded');
 
@@ -18,4 +18,44 @@ function toggleExpand() {
   for (var i=0; i<div7.length; i++) {
     div7[i].classList.toggle('show');
   }
+}
+
+function toggleExpand3left() {
+  //Check if right div extended
+  var right = document.getElementById('box3-child-right');
+  if (right.classList.contains('expanded-3')) {
+    toggleExpand3right();
+  }
+
+  //Toggle left
+  var divs = document.querySelectorAll('.expandable-3');
+  divs[0].classList.toggle('expanded-3');
+
+  //Nav
+  var navs = document.querySelectorAll('.box3-nav');
+  navs[0].classList.toggle('box3-nav-expanded');
+
+  //Text
+  var texts = document.querySelectorAll('.box3-text');
+  texts[0].classList.toggle('box3-text-expanded');
+}
+
+function toggleExpand3right() {
+  //Check if left div extended
+  var right = document.getElementById('box3-child-left');
+  if (right.classList.contains('expanded-3')) {
+    toggleExpand3left();
+  }
+
+  //Toggle right
+  var divs = document.querySelectorAll('.expandable-3');
+  divs[1].classList.toggle('expanded-3');
+
+  //Nav
+  var navs = document.querySelectorAll('.box3-nav');
+  navs[1].classList.toggle('box3-nav-expanded');
+
+  //Text
+  var texts = document.querySelectorAll('.box3-text');
+  texts[1].classList.toggle('box3-text-expanded');
 }
